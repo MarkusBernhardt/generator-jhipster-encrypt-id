6293e150dced69fbd74766fc49507ea47116481c
Markus Bernhardt <markus.bernhardt@scmb.de>
2024-04-29 16:08:27 +0200

    Server side finished

--- generators/encrypt-id-java/generator.mjs
+++ generators/encrypt-id-java/generator.mjs
@@ -57,10 +57,12 @@ export default class extends BaseApplicationGenerator {
 
   get [BaseApplicationGenerator.POST_WRITING_ENTITIES]() {
     return {
-      async postWritingEntitiesTask({ application: { mainJavaPackageDir, testJavaPackageDir }, entities }) {
+      async postWritingEntitiesTask({ application: { mainJavaPackageDir, testJavaPackageDir, packageName }, entities }) {
         for (const entity of entities.filter(e => e.enableEncryptId)) {
           this.convertDto(mainJavaPackageDir, testJavaPackageDir, entity);
-          this.convertService(mainJavaPackageDir, entity);
+          this.convertMapper(mainJavaPackageDir, packageName, entity);
+          this.convertResource(mainJavaPackageDir, packageName, entity);
+          this.convertService(mainJavaPackageDir, packageName, entity);
         }
       },
     };
@@ -115,7 +117,7 @@ export default class extends BaseApplicationGenerator {
     const dtoName = `${persistClass}DTO`;
     const dtoPath = `${mainJavaPackageDir}/${entityPackage}service/dto/${dtoName}.java`;
 
-    this.replaceLongToStringNeedles(dtoPath, ['private Long id', 'Long getId() {', 'setId(Long id) {']);
+    this.replaceLongToStringNeedles(dtoPath, ['private Long id', 'Long getId()', 'setId(Long id)']);
 
     if(!entity.builtIn){
       const dtoTestPath = `${testJavaPackageDir}/${entityPackage}service/dto/${dtoName}Test.java`;
@@ -134,41 +136,169 @@ export default class extends BaseApplicationGenerator {
     }
   }
 
-  convertService(mainJavaPackageDir, entity) {
+  convertMapper(mainJavaPackageDir, packageName, entity) {
     const { persistClass, entityPackage = '' } = entity;
+    const resourcePath = `${mainJavaPackageDir}/${entityPackage}service/mapper/${persistClass}Mapper.java`;
+
+    let regExNeedles = [
+      {
+        regex: new RegExp(`import ${packageName}.service.dto.${persistClass}DTO;$`, 'gm'),
+        content: `import ${packageName}.service.cipher.${persistClass}Cipher;\nimport ${packageName}.service.dto.${persistClass}DTO;\nimport org.springframework.beans.factory.annotation.Autowired;\n`
+      },
+      {
+        regex: new RegExp(`public interface`, 'gm'),
+        content: `public abstract class`
+      },
+      {
+        regex: new RegExp(`extends`, 'gm'),
+        content: `implements`
+      },
+      {
+        regex: new RegExp(`> \{`, 'gm'),
+        content: `> { \n@Autowired\nprotected ${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher;`
+      },
+      {
+        regex: new RegExp(`^ *([A-Za-z0-9]+ toDto[A-Za-z0-9]+)`, 'gm'),
+        content: `public abstract $1`
+      },
+      {
+        regex: new RegExp(`^ *([A-Za-z0-9]+ toEntity[A-Za-z0-9]+)`, 'gm'),
+        content: `public abstract $1`
+      },
+    ];
+
+    if(this.findRegexNeedle(resourcePath, `${persistClass} toEntity`)) {
+      regExNeedles.push(
+        {
+          regex: new RegExp(`${persistClass} toEntity`, 'gm'),
+          content: `@Mapping(target = "id", expression = "java(${changeCase.camelCase(persistClass)}Cipher.decrypt(dto.getId()))")\npublic abstract ${persistClass} toEntity`
+        }
+      );
+    }
+    else {
+      regExNeedles.push(
+        {
+          regex: new RegExp(`protected ${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher;`, 'gm'),
+          content: `protected ${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher;\n\n@Mapping(target = "id", expression = "java(${changeCase.camelCase(persistClass)}Cipher.decrypt(dto.getId()))")\npublic abstract ${persistClass} toEntity(${persistClass}DTO dto);`
+        }
+      );
+    }
+
+    if(this.findRegexNeedle(resourcePath, `${persistClass}DTO toDto`)) {
+      regExNeedles.push(
+        {
+          regex: new RegExp(`${persistClass}DTO toDto`, 'gm'),
+          content: `@Mapping(target = "id", expression = "java(${changeCase.camelCase(persistClass)}Cipher.encrypt(s.getId()))")\npublic abstract ${persistClass}DTO toDto`
+        }
+      );
+    }
+    else {
+      regExNeedles.push(
+        {
+          regex: new RegExp(`protected ${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher;`, 'gm'),
+          content: `protected ${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher;\n\n@Mapping(target = "id", expression = "java(${changeCase.camelCase(persistClass)}Cipher.encrypt(s.getId()))")\npublic abstract ${persistClass}DTO toDto(${persistClass} s);`
+        }
+      );
+    }
+
+    this.replaceRegexNeedles(resourcePath, regExNeedles);
+  }
+
+  convertResource(mainJavaPackageDir, packageName, entity) {
+    const { persistClass, entityPackage = '' } = entity;
+    const resourcePath = `${mainJavaPackageDir}/${entityPackage}web/rest/${persistClass}Resource.java`;
+
+    this.replaceLongToStringNeedles(resourcePath, ['Long id']);
 
     const regExNeedles = [
       {
-        regex: new RegExp(`import de.scmb.sultan.service.dto.${persistClass}DTO;$`, 'gm'),
-        content: `import de.scmb.sultan.service.cipher.${persistClass}Cipher;\nimport de.scmb.sultan.service.dto.${persistClass}DTO;\n`
+        regex: new RegExp(`import ${packageName}.service.dto.${persistClass}DTO;$`, 'gm'),
+        content: `import ${packageName}.service.cipher.${persistClass}Cipher;\nimport ${packageName}.service.dto.${persistClass}DTO;\n`
       },
       {
-        regex: new RegExp(`private final ${persistClass}Mapper ${changeCase.camelCase(persistClass)}Mapper;$`, 'gm'),
-        content: `private final ${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher;\n\nprivate final ${persistClass}Mapper ${changeCase.camelCase(persistClass)}Mapper;\n`
+        regex: new RegExp(`private final ${persistClass}Service ${changeCase.camelCase(persistClass)}Service;$`, 'gm'),
+        content: `private final ${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher;\n\nprivate final ${persistClass}Service ${changeCase.camelCase(persistClass)}Service;\n`
       },
       {
-        regex: new RegExp(`        ${persistClass}Mapper ${changeCase.camelCase(persistClass)}Mapper`, 'gm'),
-        content: `${persistClass}Mapper ${changeCase.camelCase(persistClass)}Mapper, ${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher`
+        regex: new RegExp(`        ${persistClass}Service ${changeCase.camelCase(persistClass)}Service`, 'gm'),
+        content: `${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher, ${persistClass}Service ${changeCase.camelCase(persistClass)}Service`
       },
       {
-        regex: new RegExp(`this.${changeCase.camelCase(persistClass)}Mapper = ${changeCase.camelCase(persistClass)}Mapper;`, 'gm'),
-        content: `this.${changeCase.camelCase(persistClass)}Mapper = ${changeCase.camelCase(persistClass)}Mapper;\nthis.${changeCase.camelCase(persistClass)}Cipher = ${changeCase.camelCase(persistClass)}Cipher;`
+        regex: new RegExp(`this.${changeCase.camelCase(persistClass)}Service = ${changeCase.camelCase(persistClass)}Service;`, 'gm'),
+        content: `this.${changeCase.camelCase(persistClass)}Cipher = ${changeCase.camelCase(persistClass)}Cipher;\nthis.${changeCase.camelCase(persistClass)}Service = ${changeCase.camelCase(persistClass)}Service;`
+      },
+      {
+        regex: new RegExp(`${changeCase.camelCase(persistClass)}Repository.existsById\\(id\\)`, 'gm'),
+        content: `${changeCase.camelCase(persistClass)}Repository.existsById(${changeCase.camelCase(persistClass)}Cipher.decrypt(id))`
+      },
+    ];
+
+    this.replaceRegexNeedles(resourcePath, regExNeedles);
+  }
+
+  convertService(mainJavaPackageDir, packageName, entity) {
+    const { persistClass, entityPackage = '' } = entity;
+
+    const servicePath = `${mainJavaPackageDir}/${entityPackage}service/${persistClass}Service.java`;
+    const serviceImplPath = `${mainJavaPackageDir}/${entityPackage}service/impl/${persistClass}ServiceImpl.java`;
+
+    this.replaceLongToStringNeedles(servicePath, ['findOne(Long id)', 'delete(Long id)']);
+    this.replaceLongToStringNeedles(serviceImplPath, ['findOne(Long id)', 'delete(Long id)']);
+
+    const regExNeedles = [
+      {
+        regex: new RegExp(`import ${packageName}.service.dto.${persistClass}DTO;$`, 'gm'),
+        content: `import ${packageName}.service.cipher.${persistClass}Cipher;\nimport ${packageName}.service.dto.${persistClass}DTO;\n`
+      },
+      {
+        regex: new RegExp(`private final ${persistClass}Repository ${changeCase.camelCase(persistClass)}Repository;$`, 'gm'),
+        content: `private final ${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher;\n\nprivate final ${persistClass}Repository ${changeCase.camelCase(persistClass)}Repository;\n`
+      },
+      {
+        regex: new RegExp(`        ${persistClass}Repository ${changeCase.camelCase(persistClass)}Repository`, 'gm'),
+        content: `${persistClass}Cipher ${changeCase.camelCase(persistClass)}Cipher, ${persistClass}Repository ${changeCase.camelCase(persistClass)}Repository`
+      },
+      {
+        regex: new RegExp(`this.${changeCase.camelCase(persistClass)}Repository = ${changeCase.camelCase(persistClass)}Repository;`, 'gm'),
+        content: `this.${changeCase.camelCase(persistClass)}Cipher = ${changeCase.camelCase(persistClass)}Cipher;\nthis.${changeCase.camelCase(persistClass)}Repository = ${changeCase.camelCase(persistClass)}Repository;`
       },
       {
         regex: new RegExp(`${changeCase.camelCase(persistClass)}DTO.getId\\(\\)`, 'gm'),
         content: `${changeCase.camelCase(persistClass)}Cipher.decrypt(${changeCase.camelCase(persistClass)}DTO.getId())`
       },
+      {
+        regex: new RegExp(`${changeCase.camelCase(persistClass)}Repository.findById\\(id\\)`, 'gm'),
+        content: `${changeCase.camelCase(persistClass)}Repository.findById(${changeCase.camelCase(persistClass)}Cipher.decrypt(id))`
+      },
+      {
+        regex: new RegExp(`${changeCase.camelCase(persistClass)}Repository.deleteById\\(id\\)`, 'gm'),
+        content: `${changeCase.camelCase(persistClass)}Repository.deleteById(${changeCase.camelCase(persistClass)}Cipher.decrypt(id))`
+      },
+      {
+        regex: new RegExp(`${changeCase.camelCase(persistClass)}Repository.findOneWithEagerRelationships\\(id\\)`, 'gm'),
+        content: `${changeCase.camelCase(persistClass)}Repository.findOneWithEagerRelationships(${changeCase.camelCase(persistClass)}Cipher.decrypt(id))`
+      },
     ];
 
-    const serviceImplPath = `${mainJavaPackageDir}/${entityPackage}service/impl/${persistClass}ServiceImpl.java`;
     this.replaceRegexNeedles(serviceImplPath, regExNeedles);
+  }
+
+  findRegexNeedle(filePath, needle) {
+    var flag = false;
+    this.editFile(filePath, content => {
+      if(content.includes(needle)) {
+        flag = true;
+      }
+      return content;
+    });
+    return flag;
   }
 
   replaceRegexNeedles(filePath, replaceNeedles) {
     replaceNeedles.forEach(needle => {
       this.editFile(filePath, contents =>
         contents
-          .replace(needle.regex, needle.content),
+          .replaceAll(needle.regex, needle.content),
       );
     });
   }
@@ -177,7 +307,7 @@ export default class extends BaseApplicationGenerator {
     replaceNeedles.forEach(needle => {
       this.editFile(filePath, contents =>
         contents
-          .replace(needle, needle.replace('Long', 'String')),
+          .replaceAll(needle, needle.replace('Long', 'String')),
       );
     });
   }
